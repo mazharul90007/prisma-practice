a4d8adb5be247e4e51420c2110aa54cf11fb111a
mazharul90007 <mazharul90007@gmail.com>
2025-06-12 20:38:13 +0600

    updated

--- src/pagination-sorting.ts
+++ src/pagination-sorting.ts
@@ -2,33 +2,32 @@ import { PrismaClient } from "../generated/prisma";
 
 const prisma = new PrismaClient();
 
-
 const paginationSorting = async () => {
-    //offset pagination
+  //offset pagination
 
-    // const offsetData = await prisma.post.findMany({
-    //     skip: 5,
-    //     take: 2
-    // })
+  // const offsetData = await prisma.post.findMany({
+  //     skip: 5,
+  //     take: 2
+  // })
 
-    //cursor based pagination
-    // const cursorData = await prisma.post.findMany({
-    //         skip: 5,
-    //         take: 2,
-    //         cursor: {
-    //             id: 15
-    //         }
-    //     })
+  //cursor based pagination
+  const cursorData = await prisma.post.findMany({
+    skip: 5,
+    take: 2,
+    cursor: {
+      id: 15,
+    },
+  });
 
-        //sorting
-        const sortedData = await prisma.post.findMany({
-            orderBy: {
-                title: 'asc'
-            },
-            skip: 3,
-            take: 5
-        })
-        console.log("sorted data", sortedData)
-    }
+  //sorting
+  // const sortedData = await prisma.post.findMany({
+  //     orderBy: {
+  //         title: 'asc'
+  //     },
+  //     skip: 3,
+  //     take: 5
+  // })
+  console.log("cursor data", cursorData);
+};
 
 paginationSorting();
